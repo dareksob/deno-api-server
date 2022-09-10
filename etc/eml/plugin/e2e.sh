3874f5ebedc4bc92b13acd3de25a7f7d6555e450
Darius Sobczak <darius@vision-release.com>
2022-09-10 00:33:43 +0000

    use fixed latest version for first run

--- etc/eml/plugin/e2e.sh
+++ etc/eml/plugin/e2e.sh
@@ -1,5 +1,5 @@
 eml_doc "e2e" "enter e2e env to run your e2e test with newman"
 function eml-extend-e2e() {
-  local service=integration-test
+  local service=e2e
   docker compose -f $dockerFile run --rm --entrypoint sh $service
 }
